54808a77083e9d1c7db2f7d3c8d57268f0ce76b7
Igor Ivanovic <igor.ivanovic@pm.me>
2019-05-12 11:13:00 +0200

    Added spring boot application, jackson dependency, logger

---
+++ exchange-api/src/main/java/io/avrios/Application.java
@@ -0,0 +1,17 @@
+package io.avrios;
+
+/**
+ * @author igorzg on 2019-05-12.
+ * @since 1.0
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class);
+  }
+}
